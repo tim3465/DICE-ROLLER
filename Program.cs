918484d590838a8f57635dafb1854c5d1aeea181
Tim Montague <tim.john.montague@gmail.com>
2023-07-19 17:24:25 -0400

    Didn't do anything extra on this one just kind of finished it

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Drawing;
 
-
 Console.WriteLine("Welcome to the Grand Circus Casino!");
-
 int diceSide = 0;
 
+//Gets the user input on how many sides the dice should have
+//the try catch won't allow anything less than 4 sides and it will only allow numbers 
 while (true)
 {
     try
@@ -27,15 +27,20 @@ while (true)
         Console.WriteLine(e);
     }
 }
+
 bool proceed = true;
 while (proceed)
 {
+    //Displays whether you win or lose dice Jardin 
     int diceOne = RandomDice(diceSide);
     int diceTwo = RandomDice(diceSide);
     Console.WriteLine($"You rolled a {diceOne} and a {diceTwo} ({diceOne + diceTwo} total)");
-    Console.WriteLine(DiceJargen(diceSide, diceOne, diceTwo) + score(diceOne,diceTwo));
+    Console.WriteLine(DiceJargin(diceSide, diceOne, diceTwo) + score(diceOne,diceTwo));
+   
+    //Gatekeeper 
+    //Gives the user an option to continue or to end the program 
     while (true)
-    {
+    {  
         Console.Write("Roll again? (y/n) :");
         string entry= Console.ReadLine().ToLower().Trim();
         if (entry =="y"||entry =="n")
@@ -56,31 +61,21 @@ while (proceed)
         }
     }
 }
-
 Console.WriteLine("Thanks for playing ");
 
-
-
 //Metheds
-
-//Create a static method to generate the random numbers.
-//Proper method header: 2 points
-//Program generates random numbers validly within the user-specified range: 1 point
-//Method returns meaningful value of proper type: 1 poin
-
+//------------------------------------------------------------------------------------------------------
+//This method generates the dice roll. 
 static int RandomDice(int sides)
 {
     Random random = new Random();
     int roll=random.Next(1,sides+1);
     return roll;
 }
-//Create a static method for six - sided dice that takes two dice values as parameters, and returns a string for one of the valid combinations(e.g.Snake Eyes, etc.) or an empty string if the dice don’t match one of the combinations.
-//Snake Eyes: Two 1s
-//Ace Deuce: A 1 and 2
-//Box Cars: Two 6s
-//Or empty string if no matching combos
 
-static string DiceJargen(int diceSide,int diceOne,int diceTwo)
+//This method Determines if you have a six sided dice
+//will display the jargon associated with the dice roll if applicable 
+static string DiceJargin(int diceSide,int diceOne,int diceTwo)
 {
     if (diceSide == 6)
     {
@@ -98,13 +93,8 @@ static string DiceJargen(int diceSide,int diceOne,int diceTwo)
         return "";
     }
 }
-//Create a static method for six - sided dice that takes two dice
-//values as parameters,
-//and returns a string for one of the valid totals(e.g.Win, etc.) or
-//an empty string if the dice don’t match one of the totals.
-//Win: A total of 7 or 11
-//Craps: A total of 2, 3, or 12
-//Or empty string if no matching combos
+
+//Will determine your score 
 static string score(int diceOne,int diceTwo)
 {
     int totle=diceOne+diceTwo;
